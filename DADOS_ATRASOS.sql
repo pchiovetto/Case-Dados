75de5d274f577b56f1000271b7a22c3f8e0222a6
pchiovetto <139031745+pchiovetto@users.noreply.github.com>
2024-10-18 17:43:57 -0300

    Update DADOS_ATRASOS.sql

--- DADOS_ATRASOS.sql
+++ DADOS_ATRASOS.sql
@@ -9,25 +9,26 @@ from tb_titulos tt)
 
 --Codigo analisando os dados, utilizando julianday, pois utilizei o dbeaver:
 SELECT tr.nm_revendedor ,
-(SELECT MAX(julianday((dt_pagamento)) - julianday((dt_vencimento)))
-FROM tb_titulo
-WHERE (dt_vencimento) >= DATE('now', '-30 days')) 
-AS MAX_DIAS_ATRASO_1M,
-(SELECT MAX(julianday((dt_pagamento)) - julianday((dt_vencimento)))
-FROM tb_titulo
-WHERE dt_vencimento >= DATE('now', '-90 days')) AS MAX_DIAS_ATRASO_3M,
-SUM(CASE 
-WHEN dt_vencimento >= DATE('now', '-90 days') 
-THEN CAST(REPLACE(vlr_pedido, ',', '.') AS REAL)
-ELSE 0 
-END) AS TOTAL_FATURADO_3M,
-COUNT(CASE 
-WHEN dt_vencimento >= DATE('now', '-90 days') 
-AND ( forma_pagamento = 'Boleto a Prazo') 
-THEN 1 
-END) AS QTD_TITULOS_BOLETO_3M 
-FROM 
-tb_titulo tt 
+
+  (SELECT MAX(julianday((dt_pagamento)) - julianday((dt_vencimento)))
+  FROM tb_titulo
+  WHERE (dt_vencimento) >= DATE('now', '-30 days')
+  ) AS MAX_DIAS_ATRASO_1M,
+
+  (SELECT MAX(julianday((dt_pagamento)) - julianday((dt_vencimento)))
+   FROM tb_titulo
+  WHERE dt_vencimento >= DATE('now', '-90 days')) AS MAX_DIAS_ATRASO_3M,
+  
+SUM(CASE WHEN dt_vencimento >= DATE('now', '-90 days') 
+     THEN CAST(REPLACE(vlr_pedido, ',', '.') AS REAL)
+     ELSE 0 
+    END) AS TOTAL_FATURADO_3M,
+
+COUNT(CASE WHEN dt_vencimento >= DATE('now', '-90 days') AND ( forma_pagamento = 'Boleto a Prazo') THEN 1 END) AS QTD_TITULOS_BOLETO_3M 
+
+FROM tb_titulo tt 
+
 left join tb_revendedor tr on tt.id_revendedor = tr.id_revendedor 
+
 GROUP by tr.nm_revendedor
 
